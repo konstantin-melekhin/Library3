e5fdc3798cf1083d5f1e1c9090868d6adde3cac8
konstantin-melekhin <65566216+konstantin-melekhin@users.noreply.github.com>
2020-05-29 01:49:34 +0200

    29.05.2020Обновлен ShiftCounter

--- Library3/ShiftCounter.vb
+++ Library3/ShiftCounter.vb
@@ -4,7 +4,7 @@
     Public Function ShiftInfo(StationID As Integer, idApp As Integer, ShiftID As Integer, LOTID As Integer) As ArrayList
         'поиск выпуска в текущую смену
         Dim ShiftCounterInfo As New ArrayList(SelectListString("USE FAS
-            SELECT [ID],[ShiftCounter],[LOT_Counter] FROM [FAS].[dbo].[FAS_ShiftsCounter]
+            SELECT [ID],[ShiftCounter],[LOT_Counter],[PassLOTRes],[FAilLOTRes] FROM [FAS].[dbo].[FAS_ShiftsCounter]
             where StationID = " & StationID & " and ID_App = " & idApp & " and LOTID = " & LOTID & " and 
             ShiftID = " & ShiftID & "  and FORMAT(CreateDate,'d', 'de-de')='" & DateTime.Today & "'"))
         Return ShiftCounterInfo
@@ -13,7 +13,6 @@
     Public Function ShiftCounterStart(StationID As Integer, idApp As Integer, LOTID As Integer) As ArrayList
         Dim ShiftID As Integer
         'определение номера смены (дневная или ночная)
-        'Dim i As Integer = CurentTimeSec
         Select Case DateAndTime.Timer
             Case 0 To 86399 ' запуск в период С 00:00:00 ДО 23:59:59
                 ShiftID = 3 'при отсутствии ночных смен
@@ -23,8 +22,8 @@
         If ShiftCounterInfo.Count = 0 Then
             'если записи нет, то делаем новую запись в таблицу счетчика за смену
             SQL = "USE FAS
-            insert into [FAS].[dbo].[FAS_ShiftsCounter] (StationID,ID_App,ShiftID,ShiftCounter,CreateDate, LOTID, LOT_Counter) 
-		    values (" & StationID & "," & idApp & "," & ShiftID & ",0,CURRENT_TIMESTAMP," & LOTID & ",0 )"
+            insert into [FAS].[dbo].[FAS_ShiftsCounter] (StationID,ID_App,ShiftID,ShiftCounter,CreateDate, LOTID, LOT_Counter,PassLOTRes,FAilLOTRes) 
+		    values (" & StationID & "," & idApp & "," & ShiftID & ",0,CURRENT_TIMESTAMP," & LOTID & ",0,0,0)"
             RunCommand(SQL)
             'повторно запрашиваем ShiftcounterID 
             ShiftCounterInfo = ShiftInfo(StationID, idApp, ShiftID, LOTID)
@@ -32,9 +31,9 @@
         Return ShiftCounterInfo
     End Function
 
-    Public Sub ShiftCounterUpdate(ShiftCounterID As String, ShiftCounter As String, LotCounter As String)
-        SQL = " Use FAS Update [FAS].[dbo].[FAS_ShiftsCounter] set ShiftCounter = " & ShiftCounter & ",
-                LOT_Counter = " & LotCounter & " where id  = " & ShiftCounterID
+    Public Sub ShiftCounterUpdateCT(ShiftCounterID As Integer, ShiftCounter As Integer, LotCounter As Integer, PassLOTRes As Integer, FAilLOTRes As Integer)
+        SQL = " Use FAS Update [FAS].[dbo].[FAS_ShiftsCounter] set ShiftCounter = " & ShiftCounter & ",LOT_Counter = " & LotCounter & "
+        ,PassLOTRes = " & PassLOTRes & ",FAilLOTRes = " & FAilLOTRes & " where id  = " & ShiftCounterID
         RunCommand(SQL)
     End Sub
 
